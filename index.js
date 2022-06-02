38c1605e71943a6ce631359a44a239273929c1d0
fedeh <fedenicohurtado@hotmail.com>
2022-06-01 23:54:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let entrada = +prompt("Simulador de elementos quimicos, ingrese la cantidad de elementos quimicos que quiere simular: ")
+for(let i = 1; i <= entrada; i++){
+    alert("elementos "+i);
+}
